315c1e9e3823aa40a0092c1fca7edfbe43cd308c
polacla1 <claus.polanka@chello.at>
2021-02-23 10:24:54 +0100

    Show last interaction in days

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,25 @@
 import com.beust.klaxon.Klaxon
 import java.io.File
 import java.time.LocalDate
+import java.time.Period
 
 fun main(args: Array<String>) {
-    when {
-        args.isNotEmpty() -> {
-            val p = Peer(args[0], LocalDate.now().toString())
-            val json = Klaxon().toJsonString(p)
-            File("./data/${p.name}.json").writeText(json)
-        }
+    if (args.isNotEmpty()) {
+        val p = Peer(args[0], LocalDate.now().toString())
+        val json = Klaxon().toJsonString(p)
+        File("./data/${p.name}.json").writeText(json)
     }
     val jsons = jsonsFrom(path = "./data")
     val peers = peersFrom(jsons)
-    peers.forEach { println(it) }
+    peers.forEach {
+        val days = toDays(it.lastInteraction)
+        val output = when {
+            days == 0 -> "today"
+            days > 1 -> "$days days ago"
+            else -> "$days day ago"
+        }
+        println("Last interaction with " + it.name + " " + output)
+    }
 }
 
 private fun jsonsFrom(path: String): List<String> {
@@ -24,6 +31,11 @@ private fun jsonsFrom(path: String): List<String> {
 
 fun peersFrom(jsons: List<String>): MutableSet<Peer> {
     return jsons.mapNotNull { Klaxon().parse<Peer>(it) }.toMutableSet()
+}
+
+private fun toDays(lastInteraction: String): Int {
+    val ld = LocalDate.parse(lastInteraction)
+    return Period.between(ld, LocalDate.now()).days
 }
 
 data class Peer(val name: String, var lastInteraction: String)
