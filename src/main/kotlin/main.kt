ab532855053a4bfb1af2315d6b0d1a517eea048b
polacla1 <claus.polanka@chello.at>
2021-02-23 12:55:28 +0100

    Add last name to avoid name clashes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,10 +4,18 @@ import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
 fun main(args: Array<String>) {
-    if (args.isNotEmpty()) {
-        val p = Peer(args[0], LocalDate.now().toString())
+    if (args.size == 2) {
+        val p = Peer(args[0], args[1], LocalDate.now().toString())
         val json = Klaxon().toJsonString(p)
-        File("./data/${p.name}.json").writeText(json)
+        File("./data/${p.lastName}_${p.firstName}.json").writeText(json)
+    }
+    if (args.size == 1) {
+        val jsons = jsonsFrom(path = "./data")
+        val peers = peersFrom(jsons)
+        val p = peers.find { it.firstName == args[0] }
+        p?.lastInteraction = LocalDate.now().toString()
+        val json = Klaxon().toJsonString(p)
+        File("./data/${p?.lastName}_${p?.firstName}.json").writeText(json)
     }
     val jsons = jsonsFrom(path = "./data")
     val peers = peersFrom(jsons)
@@ -19,7 +27,7 @@ fun main(args: Array<String>) {
             days > 1 -> "$days days ago"
             else -> "$days day ago"
         }
-        println("Last interaction with " + it.name + " " + output)
+        println("Last interaction with " + it.firstName + " " + output)
     }
 }
 
@@ -39,4 +47,4 @@ private fun toDays(lastInteraction: String): Long {
     return ChronoUnit.DAYS.between(ld, LocalDate.now())
 }
 
-data class Peer(val name: String, var lastInteraction: String)
+data class Peer(val firstName: String, val lastName: String, var lastInteraction: String)
