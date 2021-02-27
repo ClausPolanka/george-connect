6ead6f864c0f6f70c8ef8562c11442f566eb984f
Claus Polanka <claus.polanka@chello.at>
2021-02-27 12:47:26 +0100

    Reorder integrations by call order

--- src/main/kotlin/integrations.kt
+++ src/main/kotlin/integrations.kt
@@ -1,6 +1,24 @@
 import com.beust.klaxon.Klaxon
 import java.time.LocalDate
 
+fun sortedPeers(): List<Peer> {
+    val peers = peers()
+    return peers.sortedBy { toDays(it.lastInteractionF2F) }
+}
+
+private fun peers(): MutableSet<Peer> {
+    val jsons = jsonsFrom(path = "./data")
+    return peersFrom(jsons, Klaxon()::parse)
+}
+
+fun show(peers: List<Peer>, display: (s: String) -> Unit) {
+    peers.forEach {
+        val days = toDays(it.lastInteractionF2F)
+        val output = outputFor(days)
+        display("Last F2F interaction with ${it.firstName} ${it.lastName} $output")
+    }
+}
+
 fun updatePeer(args: Array<String>) {
     val (firstName, lastName) = parse(args, ::findPeerBy)
     val p = Peer(firstName, lastName, LocalDate.now().toString())
@@ -11,22 +29,4 @@ private fun findPeerBy(firstName: String): Peer? {
     val peers = peers()
     peers.throwIfDuplicatesExistFor(firstName)
     return peers.find { it.firstName == firstName }
-}
-
-private fun peers(): MutableSet<Peer> {
-    val jsons = jsonsFrom(path = "./data")
-    return peersFrom(jsons, Klaxon()::parse)
-}
-
-fun sortedPeers(): List<Peer> {
-    val peers = peers()
-    return peers.sortedBy { toDays(it.lastInteractionF2F) }
-}
-
-fun show(peers: List<Peer>, display: (s: String) -> Unit) {
-    peers.forEach {
-        val days = toDays(it.lastInteractionF2F)
-        val output = outputFor(days)
-        display("Last F2F interaction with ${it.firstName} ${it.lastName} $output")
-    }
 }
