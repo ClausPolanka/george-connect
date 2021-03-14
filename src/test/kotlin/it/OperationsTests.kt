66e1b5e84eec054ade75aa95b77369b937d8b898
polacla1 <claus.polanka@chello.at>
2021-03-14 14:24:52 +0100

    Add integration tests for all operations

---
+++ src/test/kotlin/it/OperationsTests.kt
@@ -0,0 +1,46 @@
+package it
+
+import georgeconnect.Peer
+import georgeconnect.jsonsFrom
+import georgeconnect.updateJsonFor
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.test.assertEquals
+
+class OperationsTests {
+
+    @Test
+    fun `read jsonsFrom given path`(@TempDir tempDir: Path) {
+        val jsonFile1 = tempDir.resolve("lastname1_firstname1.json")
+        val jsonFile2 = tempDir.resolve("lastname2_firstname2.json")
+        val json1 = """{"firstName" : "firstname1", "lastInteractionF2F" : "2021-03-03", "lastName" : "lastname1"}"""
+        val json2 = """{"firstName" : "firstname2", "lastInteractionF2F" : "2021-03-04", "lastName" : "lastname2"}"""
+        Files.write(jsonFile1, json1.toByteArray(Charsets.UTF_8))
+        Files.write(jsonFile2, json2.toByteArray(Charsets.UTF_8))
+
+        val jsons = jsonsFrom(path = tempDir.toString())
+
+        assertEquals(expected = listOf(json1, json2), actual = jsons, "jsons")
+    }
+
+    @Test
+    fun `UpdateJsonsFor given peer and path`(@TempDir tempDir: Path) {
+        val jsonFile = tempDir.resolve("lastname_firstname1.json")
+        val json = """{"firstName" : "firstname", "lastInteractionF2F" : "2021-03-03", "lastName" : "lastname"}"""
+        Files.write(jsonFile, json.toByteArray(Charsets.UTF_8))
+
+        updateJsonFor(
+            p = Peer("firstname", "lastname", lastInteractionF2F = "2021-03-10"),
+            path = tempDir.toString()
+        )
+
+        val actual = Files.readAllLines(jsonFile, Charsets.UTF_8)[0]
+        assertEquals(
+            expected = """{"firstName" : "firstname", "lastInteractionF2F" : "2021-03-10", "lastName" : "lastname"}""",
+            actual,
+            message = "peer json"
+        )
+    }
+}
