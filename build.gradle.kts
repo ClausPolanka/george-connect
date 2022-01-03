dcc6caa018eef7757f4793eaf2ffdd9e3226975f
Claus Polanka <claus.polanka@chello.at>
2022-01-03 23:09:16 +0100

    Bump Junit from 5.7.1 to version 5.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,9 @@ repositories {
 dependencies {
     implementation("com.beust:klaxon:5.5")
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.test {
